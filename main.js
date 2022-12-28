c9110e12847825cef578e33a53f0032166983ff8
Thomas Toy <thomastoy1213@gmail.com>
2022-12-28 01:03:09 -0800

    finished connecting API and transform data

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 import "./style.css"
+import { getWeather } from "./weather"
+import { ICON_MAP } from "./iconMap"
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+    .then(renderWeather)
+    .catch(e => {
+        console.error(e)
+        alert("Error getting weather.")
+    })
+
+function renderWeather({ current, daily, hourly }) {
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+function setValue(selector, value, { parent = document} = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src =  getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+    
+}
